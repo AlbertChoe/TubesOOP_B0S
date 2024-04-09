f90ff35b5120b8fc8a78a0432381b4c0a51a131d
FarelW <eeksapi321@gmail.com>
2024-04-10 03:21:28 +0700

    fix : sell & buy incomplete

--- src/player/Player.cpp
+++ src/player/Player.cpp
@@ -1,9 +1,9 @@
-#include "../../header/Player.hpp"
+#include "header/Player.hpp"
 
-Player::Player(string name) {
+Player::Player(string name){
     this->name=name;
-    this->weight=40;
     this->gulden=50;
+    this->weight=40;
 }
 
 void Player::eat(){
@@ -20,17 +20,17 @@ void Player::eat(){
 
         try
         {
-            Item search=this->inventory.getItem(input);
+            shared_ptr<Item> search=this->inventory.getItem(input);
 
-            if (search.getID()==0 && search.getCode()=="" )
+            if (search->getID()==0 && search->getCode()=="" )
             {
                 throw EmptySlotInputException();
-            } else if (search.getType()!="PRODUCT_ANIMAL" ||search.getType()!="PRODUCT_FOOD_PLANT")
+            } else if (search->getType()!="PRODUCT_ANIMAL" ||search->getType()!="PRODUCT_FOOD_PLANT")
             {
                 throw InvalidSlotInputException();
             }
             
-            int addedWeight = search.getAddedWeight();
+            int addedWeight = search->getAddedWeight();
             this->weight+=addedWeight;
             cout << "Dengan lahapnya, kamu memakan hidangan itu"<<endl;
             cout<<"Alhasil, berat badan kamu naik menjadi "<<this->weight<<endl;
@@ -49,71 +49,101 @@ void Player::displayInventory(){
     this->inventory.display();
 }
 
-// void Player::buy(Store& store){
+void Player::buy(Store& store){
     
-//     cout<<"Uang anda: "<<this->gulden;
-//     // cout<<"Slot penyimpanan tersedia: "<<this->inventory.sakd
+    cout<<"Uang anda: "<<this->gulden;
+    // cout<<"Slot penyimpanan tersedia: "<<this->inventory.sakd
 
-//     try
-//     {
+    try
+    {
         
-//     }
-//     catch(const exception& e)
-//     {
-//         cerr << e.what() << '\n';
-//     }
-    
-// }
+    }
+    catch(const exception& e)
+    {
+        cerr << e.what() << '\n';
+    }
+}
 
-// void Player::sell(Store& store){
-//     cout<<"Berikut merupakan penyimpanan Anda"<<endl;
-//     this->displayInventory();
-//     bool valid=false;
+string Player::getName(){
+    return this->name;
+}
 
-//     while (!valid)
-//     {
-//         string input;
-//         int tempgulden=0;
-//         cout<<"Silahkan pilih petak yang ingin Anda jual!"<<endl;
-//         cout << "Petak: ";
-//         cin >> input;
+vector<string> parseAndTrimInput(const string& input) {
+    vector<string> tokens;
+    string token;
+    for (char ch : input) {
+        if (ch == ',') {
+            if (!token.empty()) {
+                size_t first = token.find_first_not_of(' ');
+                size_t last = token.find_last_not_of(' ');
+                if (first != string::npos && last != string::npos) {
+                    tokens.push_back(token.substr(first, last - first + 1));
+                } else {
+                    tokens.push_back("");
+                }
+            } else {
+                tokens.push_back(token);
+            }
+            token.clear();
+        } else {
+            token += ch;
+        }
+    }
 
-//         int count = 1;
-//         for (char c : input) {
-//             if (c == ',') {
-//                 ++count;
-//             }
-//         }
+    if (!token.empty()) {
+        size_t first = token.find_first_not_of(' ');
+        size_t last = token.find_last_not_of(' ');
+        if (first != string::npos && last != string::npos) {
+            tokens.push_back(token.substr(first, last - first + 1));
+        } else {
+            tokens.push_back("");
+        }
+    }
+    return tokens;
+}
 
-//         string* parsedInput = new string[count];
+void Player::sell(Store& store){
+    cout<<"Berikut merupakan penyimpanan Anda"<<endl;
+    this->displayInventory();
+    bool valid=false;
 
-//         cout<<endl;
+    while (!valid)
+    {
+        string input;
+        int tempgulden=0;
+        cout<<"Silahkan pilih petak yang ingin Anda jual!"<<endl;
+        cout << "Petak: ";
+        getline(cin, input);
 
-//         try
-//         {
-//             Item search=this->inventory.getItem(input);
+        vector<string> parsedInput = parseAndTrimInput(input);
 
-//             if (search.getID()==0 && search.getCode()=="" )
-//             {
-//                 throw EmptySlotSellException();
-//             }
+        cout<<endl;
 
-//             tempgulden+=this->inventory.getItem(input).getPrice();
-//             this->gulden+=tempgulden;
+        try
+        {
+            shared_ptr<Item> search=this->inventory.getItem(input);
+
+            if (search->getID()==0 && search->getCode()=="")
+            {
+                throw EmptySlotSellException();
+            }
+
+            tempgulden+=this->inventory.getItem(input)->getPrice();
+            this->gulden+=tempgulden;
             
-//             this->inventory.removeItem(input);
+            this->inventory.removeItem(input);
 
-//             cout<<endl<<"Barang Anda berhasil dijual! Uang Anda bertambah"<<tempgulden<<"gulden!"<<endl;
+            cout<<endl<<"Barang Anda berhasil dijual! Uang Anda bertambah"<<tempgulden<<"gulden!"<<endl;
 
-//             valid=true;
+            valid=true;
             
-//         }
-//         catch(const exception& e)
-//         {
-//             cerr << e.what() << '\n';
-//         }
-//     }
-// }
+        }
+        catch(const exception& e)
+        {
+            cerr << e.what() << '\n';
+        }
+    }
+}
 
 int Player::getGulden() {
     return this->gulden;
