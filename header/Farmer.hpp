93fa36baf8cb3dcbf2722bd67ca4226ac584aea4
AlbertChoe <albert.choe73@gmail.com>
2024-04-13 16:36:51 +0700

    remove unused

--- header/Farmer.hpp
+++ header/Farmer.hpp
@@ -12,7 +12,6 @@ private:
 public:
     Farmer( string name, int InventoryRow, int InventoryCol, int fieldRow, int fieldCol): Player(name,InventoryRow,InventoryCol), field(fieldRow, fieldCol) {}
     Farmer( string name, int weight, int gulden, int InventoryRow, int InventoryCol, int fieldRow, int fieldCol): Player(name, weight, gulden, InventoryRow, InventoryCol), field(fieldRow, fieldCol) {}
-    PlayerType getType();
     void plantCrop();
     void displayField();
     void harvestCrop();
